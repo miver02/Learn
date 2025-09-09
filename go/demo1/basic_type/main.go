9591aa7f0c108801c2efba1d56312622d134beab
miver02 <201900465@qq.com>
2025-09-09 09:05:51 +0800

    go update

---
+++ go/demo1/basic_type/main.go
@@ -0,0 +1,27 @@
+package main
+
+
+func main() {
+	var a int = 234
+	var b int =345
+	println(a + b)
+	println(a - b)
+	println(a * b)
+	if b != 0 {
+		println(a / b)
+		println(a % b)
+	}
+
+	// var  c float64 = 12.3
+	// println(a + c)
+	
+	// var d int32 = 12
+	// println(a + b)
+
+	String()
+
+
+
+}
+	
+
---
+++ go/demo1/basic_type/string.go
@@ -0,0 +1,11 @@
+package main
+
+
+func String() {
+	println("hello" + "go")
+	println(len("abc"))
+	//println(len("ä½ å"))
+       	//println(utf8.RuneCountInString("ä½ å¥"))
+
+
+}
