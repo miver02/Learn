bfe366d895226bc32b6e32dea213c8bdc4cb7c17
miver <201900465@qq.com>
2025-09-11 13:10:34 +0800

    提交

--- fastapi/test.py
+++ fastapi/test.py
@@ -1,24 +1,32 @@
-from jose import jwt
-from datetime import datetime, timedelta
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        results = []
+        list1 = []
+        num = 0
+        for i in range(len(s)):
+            num = i
+            while True:
+                if num == len(s) or s[num] in list1:
+                    results.append(len(list1))
+                    list1.clear()
+                    break
+                list1.append(s[num])
+                num += 1
 
-# 配置参数
-SECRET_KEY = "800043f784d95341c66fabd500335ca3ce7fe4098cfbaedcd05646417d9a3872"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+        return max(results) if results else 0
 
-def create_jwt_token(data: dict):
-    # 复制数据以避免修改原始字典
-    to_encode = data.copy()
-    
-    # 设置令牌过期时间
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    
-    # 生成JWT令牌
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        results, list1 = [], []
+        num, result= 0, 0
+        for i in range(len(s)):
+            if i != 0:
+                list1.remove(s[i-1])
+            while num < len(s) and s[num] not in list1:
+                list1.append(s[num])
+                num += 1
+            result = max(result, num-i)
+        return result
 
-# 加密"123456"，这里将其作为subject存储在令牌中
-token = create_jwt_token(data={"sub": "123"})
-print("加密后的JWT令牌：")
-print(token)
+
+        
