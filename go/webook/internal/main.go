a8c992a13918ed7334757f89a1f0017c5f291efe
miver <201900465@qq.com>
2025-09-15 11:00:57 +0800

    提交

--- go/webook/internal/main.go
+++ go/webook/internal/main.go
@@ -1,11 +1,34 @@
 package main
 
-import "github.com/miver02/Learn/go/webook/internal/web"
+import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/miver02/Learn/go/webook/internal/web"
+)
+
+
 
 
 func main() {
 	api := web.RegisterRoutes()
 
+	// 解决跨域问题
+	api.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"https://foo.com"},
+		// AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		// ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "xxx.com'")
+		},
+		MaxAge: 12 * time.Hour,
+ 	  }))
 
 	api.Run(":8001")
-}
+} 
