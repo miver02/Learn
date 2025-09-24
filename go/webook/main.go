d714452ef83ad31c69ce62a52aa520351967a23c
miver <201900465@qq.com>
2025-09-24 10:18:58 +0800

    使用k8s练习

--- go/webook/main.go
+++ go/webook/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"github.com/gin-contrib/sessions"
+	"net/http"
+
+	// "github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 
-	"github.com/miver02/Learn/go/webook/internal/repository"
-	"github.com/miver02/Learn/go/webook/internal/web"
+	// "github.com/miver02/Learn/go/webook/internal/repository"
+	// "github.com/miver02/Learn/go/webook/internal/web"
 )
 
 
 func main() {
 	// 创建一个默认的 HTTP 服务器实例
 	api := gin.Default()
-
+/*
 	// 数据库层
 	db := repository.NewInitDatebase().InitDB()
 	rdb := repository.NewInitDatebase().InitRedis()
@@ -29,7 +31,10 @@ func main() {
 
 	// 路由层
 	api = web.RegisterRoutes(db, api)
-
+*/
+	api.GET("/hello", func(ctx *gin.Context)  {
+		ctx.String(http.StatusOK, "你好,k8s")
+	})
 	// 启动地址
 	api.Run(":8001")
 } 
