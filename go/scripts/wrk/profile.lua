95395794f947b79d1b12762e54b7bc9a1b478d86
miver <201900465@qq.com>
2025-10-20 13:32:20 +0800

    修改测试数据

--- go/scripts/wrk/profile.lua
+++ go/scripts/wrk/profile.lua
@@ -1,5 +1,5 @@
 wrk.method="GET"
 wrk.headers["Content-Type"] = "application/json"
-wrk.headers["User-Agent"] = "PostmanRuntime/7.32.3"
+wrk.headers["User-Agent"] = "Apifox/1.0.0 (https://apifox.com)"
 -- 记得修改这个，你在登录页面登录一下，然后复制一个过来这里
-wrk.headers["Authorization"]="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6NiwiVXNlckFnZW50IjoiUG9zdG1hblJ1bnRpbWUvNy4zMi4zIiwiZXhwIjoxNjkwMjczNjUwfQ.qmZ2jwT-JxDy4uGpuKJLSudEDpoxC1FDOe_KciNZbO8"
+wrk.headers["Authorization"]="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjA5Mzg4NzcsIlVpZCI6MSwiVXNlckFnZW50IjoiQXBpZm94LzEuMC4wIChodHRwczovL2FwaWZveC5jb20pIn0.OswbZZMT_5p05nj8xGBML4qrakvY-7smXXwzx0IdzfTI5CGFkdlvpEzZ1dTj7qtYFgXQ0VvP9-VuRwtRp0LTeg"
